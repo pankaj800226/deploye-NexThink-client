91ffc8c8f60f3cd91350d3ae57fa69de5db78877
Pankaj kumar <pankajsingh@Mac.lan>
2026-01-30 15:30:15 +0530

    new feature added

---
+++ dist/assets/index-CCZuYV6u.js
@@ -0,0 +1,1 @@
+import{a2 as r,y as a}from"./index-D-aXfya8.js";var t=r();const e=a(t);export{e as R,t as r};
---
+++ dist/assets/useThemeProps-BySiHRHb.js
@@ -0,0 +1,1 @@
+import{N as m}from"./index-D-aXfya8.js";import{g as a}from"./getThemeProps-CP5Hsce3.js";import{a as p}from"./Paper-Dd2lGMcw.js";const n=m();function c({props:s,name:t,defaultTheme:o,themeId:r}){let e=p(o);return r&&(e=e[r]||e),a({theme:e,name:t,props:s})}export{n as s,c as u};
