c6358cc3c3f3c06df3cc2755df50eac78854d129
Pankaj kumar <pankajsingh@Mac.lan>
2026-01-30 20:34:59 +0530

    new feature added

---
+++ dist/assets/useThemeProps-BUwU-P7r.js
@@ -0,0 +1,1 @@
+import{N as m}from"./index-kpstPohF.js";import{g as a}from"./getThemeProps-CoEtZgGU.js";import{a as p}from"./Paper-D_1wMHUg.js";const n=m();function c({props:s,name:t,defaultTheme:o,themeId:r}){let e=p(o);return r&&(e=e[r]||e),a({theme:e,name:t,props:s})}export{n as s,c as u};
