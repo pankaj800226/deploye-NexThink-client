f7fcb1192e5f5be2f68c46bbad84e238a43fc849
Pankaj kumar <pankajsingh@Mac.lan>
2026-04-26 14:38:11 +0530

    new code added

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,2 +1,2 @@
-// export const api = "https://nexthinks-backend-o6no.onrender.com";
-export const api = "http://localhost:8000"; 
+export const api = "https://nexthinks-backend-o6no.onrender.com";
+// export const api = "http://localhost:8000"; 
